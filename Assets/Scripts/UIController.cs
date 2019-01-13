1b7413ad4be90fee43141f4c8fef1a7e2338cf08
Austin Kellar <ajk@Austins-MacBook-Pro-2.local>
2019-01-12 17:45:30 -0800

    added ability to  save and load configurations

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -25,13 +26,18 @@ public class UIController : MonoBehaviour
 
     // shape
     private Dropdown shapeDropdown;
-
     public enum ShapeOption
     {
         Cube,
         Sphere,
         Capsule
     }
+
+    // save / load
+    private Dropdown loadDropdown;
+    private Button loadButton;
+    private Button saveButton;
+    private List<Configuration> configurations;
 
     public Vector3 Position
     {
@@ -108,10 +114,79 @@ public class UIController : MonoBehaviour
 
         // shape
         shapeDropdown = GameObject.Find("Shape").GetComponent<Dropdown>();
+
+        // save / load
+        loadDropdown = GameObject.Find("LoadOptions").GetComponent<Dropdown>();
+        loadButton = GameObject.Find("Load").GetComponent<Button>();
+        saveButton = GameObject.Find("Save").GetComponent<Button>();
+        loadButton.onClick.AddListener(ClickLoad);
+        saveButton.onClick.AddListener(ClickSave);
+        configurations = new List<Configuration>();
 	}
 	
-	void Update () 
+	void ClickLoad()
     {
-  
-	}
+        if (configurations.Count == 0)
+        {
+            return;
+        }
+
+        var selectedConfiguration = configurations[loadDropdown.value];
+
+        // position
+        xPositionSlider.value = selectedConfiguration.position.x;
+        yPositionSlider.value = selectedConfiguration.position.y;
+        zPositionSlider.value = selectedConfiguration.position.z;
+
+        // rotation
+        xRotationSlider.value = selectedConfiguration.rotation.x;
+        yRotationSlider.value = selectedConfiguration.rotation.y;
+        zRotationSlider.value = selectedConfiguration.rotation.z;
+
+        // scale
+        xScaleSlider.value = selectedConfiguration.scale.x;
+        yScaleSlider.value = selectedConfiguration.scale.y;
+        zScaleSlider.value = selectedConfiguration.scale.z;
+
+        // color
+        if (selectedConfiguration.color == Color.blue)
+        {
+            colorDropdown.value = 0;
+        }
+        else if (selectedConfiguration.color == Color.red)
+        {
+            colorDropdown.value = 1;
+        }
+        else if (selectedConfiguration.color == Color.green)
+        {
+            colorDropdown.value = 2;
+        }
+
+        // shape
+        shapeDropdown.value = Convert.ToInt32(selectedConfiguration.shape);
+    }
+
+    void ClickSave()
+    {
+        configurations.Add(new Configuration(Position, Rotation, Scale, Color, Shape));
+        loadDropdown.AddOptions(new List<string> { "Configuration " + configurations.Count });
+    }
+
+    private class Configuration
+    {
+        public Vector3 position;
+        public Vector3 rotation;
+        public Vector3 scale;
+        public Color color;
+        public ShapeOption shape;
+
+        public Configuration(Vector3 position, Vector3 rotation, Vector3 scale, Color color, ShapeOption shape)
+        {
+            this.position = position;
+            this.rotation = rotation;
+            this.scale = scale;
+            this.color = color;
+            this.shape = shape;
+        }
+    }
 }
